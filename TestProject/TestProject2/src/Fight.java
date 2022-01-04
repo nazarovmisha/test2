60a235328bc8b7e0469733216c1b23acf0ee59ea
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-01-04 13:52:59 +0300

    Test project

---
+++ TestProject/TestProject2/src/Fight.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Fight {
+    public static void main(String[] args) {
+
+
+        int[] arr = {5, 24, 3, 66, 38, 16, 27};
+        Arrays.sort(arr);
+        System.out.println(arr[2]);
+
+
+    }
+}
+
+
