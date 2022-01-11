95f8ad26c471d459be72338cd100f2c911921a46
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-01-11 20:30:28 +0300

    Test1

--- TestProject/Tasks/src/HomeWork/ComparatorByLength.java
+++ TestProject/Tasks/src/HomeWork/ComparatorByLength.java
@@ -7,25 +7,26 @@ import java.util.Comparator;
 public class ComparatorByLength implements Comparator<String> {
     String[] arr;
 
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+    }
 
-        @Override
-        public int compare (String arg0, String arg1){
-            if (arg0 == null)
-                return 1;
-            if (arg1 == null)
-                return -1;
-            return arg0.length() - arg1.length();
-        }
+    @Override
+    public int compare(String arg0, String arg1) {
+        if (arg0 == null)
+            return 1;
+        if (arg1 == null)
+            return -1;
+        return arg0.length() - arg1.length();
+    }
 
-        public void сomparatorByLength (String[]arr){
-            this.arr =  arr;
-        }
+    public void сomparatorByLength(String[] arr) {
+        this.arr = arr;
+    }
 
-        public void  sortArr () {
+    public void sortArr() {
         Arrays.sort(arr, new ComparatorByLength());
         System.out.println(Arrays.toString(arr));
-        }
     }
+}
 
 
