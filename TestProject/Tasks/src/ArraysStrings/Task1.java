5d8744a2b096b6e594c99aac4f7f5d8935fa2b3e
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-01-23 20:15:15 +0300

    task

---
+++ TestProject/Tasks/src/ArraysStrings/Task1.java
@@ -0,0 +1,14 @@
+package ArraysStrings;
+
+import java.util.Arrays;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 24, 3, 66, 38, 16, 27};
+        Arrays.sort(arr);
+        System.out.println(arr[2]);
+
+    }
+}
