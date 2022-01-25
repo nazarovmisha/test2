ab49cfa1a0df1c38e48543d89bc13ac4dc07915c
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-01-25 20:24:53 +0300

    horse

--- TestProject/Tasks/src/Chess/Horse.java
+++ TestProject/Tasks/src/Chess/Horse.java
@@ -1,22 +1,51 @@
 package Chess;
 
-public abstract class Horse extends ChessPiece {
+public class Horse extends ChessPiece {
 
 
     public Horse(String color) {
         super(color);
     }
 
+    @Override
     public String getColor() {
-        return color;
+        return this.color;
     }
 
-    public boolean canMoveToPosition(int line, int column, int toLine, int toColumn, ChessBoard board) {
-        if (!board.checkPos(toLine) && !board.checkPos(toColumn)) {
-            return false;
-        }
-        return true;
+    @Override
+    public boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn) {
+        if (checkPos(line) && checkPos(column) && checkPos(toLine) && checkPos(toColumn)) {
+            if (line != toLine && column != toColumn && (chessBoard.board[toLine][toColumn] == null ||
+                    !chessBoard.board[toLine][toColumn].color.equals(this.color)) &&
+                    chessBoard.board[line][column] != null) {
+                if (!chessBoard.board[line][column].equals(this)) {
+                    return false;
+                }
+            }
 
+            int[][] positions = new int[][]{
+                    {line - 2, column - 1}, {line - 2, column + 1},
+                    {line + 2, column - 1}, {line + 2, column + 1},
+                    {line - 1, column - 2}, {line - 1, column + 2},
+                    {line + 1, column - 2}, {line + 1, column + 2}};
+
+            for (int i = 0; i< positions.length; i++){
+                if(positions[i][0]==toLine&&positions[i][1]==toColumn){
+                    return true;
+                }
+            }
+        } else return false;
+        return false;
+    }
+@Override
+    public String getSymbol(){
+        return "H";
+    }
+
+    public boolean checkPos(int pos) {
+        if (0 <= pos && pos <= 7) {
+            return true;
+        } else return false;
     }
 }
 
