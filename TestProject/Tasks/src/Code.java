c26a96fb53a971604741e33a45ab8a1b8413638d
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-01-30 15:45:02 +0300

    decoder

--- TestProject/Tasks/src/Code.java
+++ TestProject/Tasks/src/Code.java
@@ -3,19 +3,19 @@ public class Code {
 
     public static void main(String[] args) {
         Code c1 = new Code();
-       c1.replaceAll();
-       c1.decoder();
+        c1.replaceAll();
+        c1.decoder();
     }
 
-    public  void replaceAll() {
-      code =  code.replaceAll("e", "Xww12").replaceAll("s", "Bmmj44")
+    public void replaceAll() {
+        code = code.replaceAll("e", "Xww12").replaceAll("s", "Bmmj44")
                 .replaceAll("c", "rtt").replaceAll("a", "qwerasdf");
 
         System.out.println(code);
     }
 
     public void decoder() {
-       code = code.replaceAll("Xww12", "e").replaceAll("Bmmj44", "s")
+        code = code.replaceAll("Xww12", "e").replaceAll("Bmmj44", "s")
                 .replaceAll("rtt", "c").replaceAll("qwerasdf", "a");
         System.out.println(code);
 
