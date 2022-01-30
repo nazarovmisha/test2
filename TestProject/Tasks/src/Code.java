52356215f0c91a22ce247c403dee39f43693e40a
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-01-30 15:40:32 +0300

    decoder

--- TestProject/Tasks/src/Code.java
+++ TestProject/Tasks/src/Code.java
@@ -1,24 +1,24 @@
-
 public class Code {
     String code = "secret message";
 
     public static void main(String[] args) {
+        Code c1 = new Code();
+       c1.replaceAll();
+       c1.decoder();
     }
 
-    public void replaceAll() {
-        code.replaceAll("e", "Xww12").replaceAll("s", "Bmmj44")
+    public  void replaceAll() {
+      code =  code.replaceAll("e", "Xww12").replaceAll("s", "Bmmj44")
                 .replaceAll("c", "rtt").replaceAll("a", "qwerasdf");
 
         System.out.println(code);
     }
 
     public void decoder() {
-        code.replaceAll("Xww12", "e").replaceAll("Bmmj44", "s")
-                .replaceAll("rtt", "s").replaceAll("qwerasdf", "a");
+       code = code.replaceAll("Xww12", "e").replaceAll("Bmmj44", "s")
+                .replaceAll("rtt", "c").replaceAll("qwerasdf", "a");
         System.out.println(code);
 
     }
 }
-
-
 
---
+++ TestProject/Tasks/src/Code1.java
@@ -0,0 +1,13 @@
+public class Code1 {
+    public static void main(String[] args) {
+
+    }
+
+    public void replace(){
+        String [] arr= {"secret massege"};
+     //   String  [] code = arr.charArray();
+
+
+
+    }
+}
