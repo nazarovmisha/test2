8effcd2879448e585d51c7871eb111dff5f5ab4c
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-02-07 19:48:29 +0300

    task4

---
+++ TestProject/Tasks/src/SF/IntupOutput/Task4.java
@@ -0,0 +1,32 @@
+package SF.IntupOutput;
+
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Task4 {
+
+    public void writeOrder() throws IOException {
+        Scanner scanner = new Scanner(new FileInputStream("Root/src/numbers.txt"));
+        int index = 1;
+        String resString = "";
+        while (scanner.hasNextLine()) {
+            String nowNumber = scanner.nextLine();
+            if (index == Integer.parseInt(nowNumber)) ;
+            resString += nowNumber + " ";
+
+            index++;
+        }
+
+        if (resString.equals("")) {
+            resString = "0";
+        }
+
+        FileWriter writer = new FileWriter("outpoot.txt", false);
+        writer.write(resString.trim());
+        writer.flush();
+
+    }
+}
+
