3ce335fe67d5090d4737174523d1500db9bf89cb
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-02-10 19:19:46 +0300

    task8

--- TestProject/Tasks/src/SF/IntupOutput/Task8.java
+++ TestProject/Tasks/src/SF/IntupOutput/Task8.java
@@ -1,4 +1,20 @@
 package SF.IntupOutput;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task8 {
+
+    public void solution(Double n) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new FileInputStream("Root/src/EGE.txt"));
+        String[] header = scanner.nextLine().split(";");
+        
+        while (scanner.hasNextLine()) {
+            String[] element = scanner.nextLine().split(";");
+            double percents = Double.parseDouble(element[2]);
+            if (percents >= n) System.out.println(element[0]);
+        }
+    }
 }
+
