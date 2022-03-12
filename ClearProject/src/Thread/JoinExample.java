c629c8b3a6327b620ef52d4d0d4abe3d8a013791
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-03-12 18:49:04 +0300

    JoinExample

--- ClearProject/src/Thread/JoinExample.java
+++ ClearProject/src/Thread/JoinExample.java
@@ -1,20 +1,19 @@
 package Thread;
 
 
-    public class JoinExample {
-
-        private static int counter = 0;
-        public static void main(String[] args) throws InterruptedException {
-            new Thread() {
-                public void run(){
-                    for (int i = 0; i < 50000000; i++) {
-                        JoinExample.counter++;
-                    }
+public class JoinExample {
+    private static int counter = 0;
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread() {
+            public void run() {
+                for (int i = 0; i < 1000000000; i++) {
+                    JoinExample.counter++;
                 }
-            }.start();
-
-            System.out.println("Counter value: " + counter); // Counter value: 0
-        }
-
+            }
+        };
+        thread.start();
+        thread.join(); // main() поток блокируется и ждет, пока не завершится поток thread
+        System.out.println("Counter value: " + counter); // Counter value: 1000000000
     }
+}
 
