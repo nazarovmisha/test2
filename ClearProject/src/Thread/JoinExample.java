6e3e75f6653853945a681fa638f9e7f79f0b70e6
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-03-12 18:47:44 +0300

    JoinExample

---
+++ ClearProject/src/Thread/JoinExample.java
@@ -0,0 +1,20 @@
+package Thread;
+
+
+    public class JoinExample {
+
+        private static int counter = 0;
+        public static void main(String[] args) throws InterruptedException {
+            new Thread() {
+                public void run(){
+                    for (int i = 0; i < 50000000; i++) {
+                        JoinExample.counter++;
+                    }
+                }
+            }.start();
+
+            System.out.println("Counter value: " + counter); // Counter value: 0
+        }
+
+    }
+
