520ab1c49824ae40826b053aca53398dbbff5ba1
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-03-14 18:09:05 +0300

    Main1

---
+++ ClearProject/src/Thread/Main1.java
@@ -0,0 +1,16 @@
+package Thread;
+
+public class Main1 extends Thread {
+
+    public static void main(String[] args) {
+
+
+        Thread thread = new LoadingThread();
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {}
+
+        System.out.println("Images are loaded");
+    }
+}
