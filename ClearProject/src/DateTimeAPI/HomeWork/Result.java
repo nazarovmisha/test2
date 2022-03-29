ae9802e2e6ffe23ef1b0d64a690d200cdbe23153
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-03-29 19:59:50 +0300

    Result

--- ClearProject/src/DateTimeAPI/HomeWork/Result.java
+++ ClearProject/src/DateTimeAPI/HomeWork/Result.java
@@ -1,14 +1,4 @@
 package DateTimeAPI.HomeWork;
-//Создайте файл result.txt в текущей директории со следующим содержимым:
-//duck
-//duck
-//duck
-//goose
-//monkey
-//gorilla
-//bonobo
-//mama gorilla
-//baby gorilla
 
 import java.io.IOException;
 import java.nio.charset.Charset;
@@ -18,7 +8,6 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 public class Result {
     public static void main(String[] args) {
         final Path text = Paths.get("d:\\result.txt");
@@ -26,9 +15,10 @@ public class Result {
         try {
             final List<String> listMonkey = Files.readAllLines(text, Charset.defaultCharset());
 
-             List<String> sort = listMonkey
-                    .stream().
-                    distinct().sorted()
+          final  List<String> sort = listMonkey
+                    .stream()
+                    .distinct()
+                    .sorted()
                     .collect(Collectors.toList());
             sort.forEach(System.out::println);
         } catch (IOException e) {
