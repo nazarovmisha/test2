1269b4bb07a84d0aea843fd6e93b00eeeb631267
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-04-02 15:38:29 +0300

    TreeExample

---
+++ ClearProject/src/JavaMemory/TreeExample.java
@@ -0,0 +1,43 @@
+package JavaMemory;
+
+public class TreeExample {
+    static class Wallet {
+        double money;
+    }
+
+    static class Phone {
+        double chargeLevel;
+    }
+
+    static class Backpack {
+        Wallet wallet;
+        Phone phone;
+    }
+
+    static class Person {
+        String name;
+        Backpack backpack;
+    }
+
+    public static void main(String[] args) {
+        Person person = createPerson();
+    }
+
+    private static Person createPerson() {
+        Phone phone = new Phone();
+        phone.chargeLevel = 75;
+
+        Wallet wallet = new Wallet();
+        wallet.money = 142;
+
+        Backpack backpack = new Backpack();
+        backpack.wallet = wallet;
+        backpack.phone = phone;
+
+        Person person = new Person();
+        person.name = "Eugene";
+        person.backpack = backpack;
+
+        return person; // 1
+    }
+}
