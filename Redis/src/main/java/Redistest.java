0a3ea17629cc07e73cde675242c8842904626cc4
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-05-29 13:59:32 +0300

    Redistest

--- Redis/src/main/java/Redistest.java
+++ Redis/src/main/java/Redistest.java
@@ -4,5 +4,13 @@ public class Redistest {
     public static void main(String[] args) {
      Jedis jedis = new Jedis();
         System.out.println(jedis.ping());
+        jedis.set("testkey", "testvalue");
+        System.out.println("Метод для проверки наличия индекса");
+        System.out.println(jedis.exists("testkey"));
+        System.out.println(jedis.exists("Falsetestkey"));
+        System.out.println("Метод добавления словаря");
+        System.out.println(jedis.hset("Fild","TestSetKey", "Value"));
+        System.out.println(jedis.hset("another","TestSetKey", "testvalue"));
+        System.out.println(jedis.hget("TestSetKey", "another"));
     }
 }
