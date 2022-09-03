5fb50bde6e43704d2dc114b61041a982e8d333a6
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-09-03 10:24:06 +0300

    Cars

--- TestProject/Tasks/src/SF/IntupOutput/Solution.java
+++ TestProject/Tasks/src/SF/IntupOutput/Solution.java
@@ -1,4 +1,16 @@
 package SF.IntupOutput;
 
+import java.util.Arrays;
+
 public class Solution {
+    public static void main(String[] args) {
+        String str = "camelCasingTest";
+        camelCase(str);
+    }
+    public static String camelCase(String input) {
+        String[]result=input.split("(?=[A-Z])");
+       String finalstring = String.join(" ",result);
+       return finalstring;
+    }
+
 }
