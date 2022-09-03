00cd4d6de67be80bc283b303518b0299a81a5be0
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-09-03 14:12:22 +0300

    summ

--- TestProject/Tasks/src/SF/IntupOutput/NthSeries.java
+++ TestProject/Tasks/src/SF/IntupOutput/NthSeries.java
@@ -1,4 +1,25 @@
 package SF.IntupOutput;
 
+
 public class NthSeries {
+    public static void main(String[] args) {
+        int z = 24;
+        seriesSum(z);
+    }
+
+    public static String seriesSum(int n) {
+        double[] arr = new double[n];
+        double x = 0;
+        double result = 0;
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = 1 / (1 + x);
+            x += 3;
+            result += arr[i];
+        }
+
+        String output = String.format("%.2f",result);
+       System.out.println(output);
+        return output;
+    }
 }
