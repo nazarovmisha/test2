5aa6db92aedd812166efd8cf01d86f470388c94e
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-09-03 12:10:38 +0300

    Cars

--- TestProject/Tasks/src/SF/IntupOutput/Troll.java
+++ TestProject/Tasks/src/SF/IntupOutput/Troll.java
@@ -1,4 +1,13 @@
 package SF.IntupOutput;
 
 public class Troll {
+    public static void main(String[] args) {
+        String input ="No offense but,\nYour writing is among the worst I've ever read";
+        disemvowel(input);
+    }
+    public static String disemvowel(String str) {
+    str = str.replaceAll("[euoiaEUOIA]","");
+        return str;
+
+    }
 }
