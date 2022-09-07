851508f6b01534660563348ef0e2bdf65b1efcd6
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-09-07 22:24:55 +0300

    CountDig.java

--- TestProject/Tasks/src/SF/IntupOutput/CountDig.java
+++ TestProject/Tasks/src/SF/IntupOutput/CountDig.java
@@ -1,40 +1,48 @@
 package SF.IntupOutput;
-
-import java.awt.font.FontRenderContext;
-import java.util.Arrays;
+//Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+//
+//        Square all numbers k (0 <= k <= n) between 0 and n.
+//
+//        Count the numbers of digits d used in the writing of all the k**2.
+//
+//        Call nb_dig (or nbDig or ...) the function taking n and d as parameters and returning this count.
+//
+//        Examples:
+//        n = 10, d = 1
+//        the k*k are 0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+//        We are using the digit 1 in: 1, 16, 81, 100. The total count is then 4.
+//
+//        nb_dig(25, 1) returns 11 since
+//        the k*k that contain the digit 1 are:
+//        1, 16, 81, 100, 121, 144, 169, 196, 361, 441.
+//        So there are 11 digits 1 for the squares of numbers between 0 and 25.
+//        Note that 121 has twice the digit 1.
 
 public class CountDig {
     public static void main(String[] args) {
-        int n = 10;
+        int n = 11549;
         int d = 1;
-        int i = 0;
         nbDig(n, d);
     }
 
     public static int nbDig(int n, int d) {
         int tmp = 0;
-        int a = 0;
+        String arrayString = "";
 
-        char[]ch =new char[10000];
-        int[] newarr = new int[n + 1];
+
         for (int i = 0; i <= n; i++) {
-            newarr[i] = i * i;
+            arrayString += "" + i * i;
         }
-        for(int i=0;i<newarr.length;i++){
 
-            while(a>0) {
-                i.insert(0, a%10);
-                a = a / 10;
+        int[] newarr= new int [arrayString.length()];
+        for (int j = 0; j < arrayString.length(); j++) {
+            newarr[j] = arrayString.charAt(j) - '0';
+            if (newarr[j] == d) {
+                tmp++;
             }
         }
 
 
-
-        System.out.println(Arrays.toString(newarr));
         return tmp;
     }
-
 }
-
-
-
