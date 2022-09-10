8ced2feecf3a4e56637b77736641450fcad63831
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-09-10 10:44:44 +0300

    StringEqualsIgnoreCase

---
+++ TestProject/Tasks/src/SF/IntupOutput/StringEqualsIgnoreCase.java
@@ -0,0 +1,9 @@
+package SF.IntupOutput;
+
+public class StringEqualsIgnoreCase {
+    public static void main(String[] args) {
+
+        String s1 = "Vasilii Ivanov";
+        System.out.println(s1.equalsIgnoreCase("vasilii ivanov"));
+    }
+}
