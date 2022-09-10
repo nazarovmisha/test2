e53220784fcf47cff2f30c90a2592f7cae6dbd70
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-09-10 17:22:51 +0300

    For

---
+++ TestProject/Tasks/src/SF/IntupOutput/For.java
@@ -0,0 +1,21 @@
+package SF.IntupOutput;
+
+import java.util.Arrays;
+
+public class For {
+    public static int[] numbers = new int[100];
+
+    public static void main(String[] args) {
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i]=i+1;
+            if(numbers[i]%2==0){
+                numbers[i]=i-i*2-1;
+            }
+
+
+        }
+        System.out.println(Arrays.toString(numbers));
+    }
+}
+
+
