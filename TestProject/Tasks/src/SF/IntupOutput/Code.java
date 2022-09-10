f7616a79690e24cf8a4a450fefeb39996ff85935
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-09-10 22:13:48 +0300

    code

---
+++ TestProject/Tasks/src/SF/IntupOutput/Code.java
@@ -0,0 +1,18 @@
+package SF.IntupOutput;
+
+public class Code {
+    public static String text = "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. It is also used to temporarily replace text in a process called greeking, which allows designers to consider the form of a webpage or publication, without the meaning of the text influencing the design.";
+
+    public static String encrypt(String text, int shift) {
+      return   // code here...
+    }
+
+    public static String decrypt(String text, int shift) {
+       return  // code here...
+    }
+
+    public static void main(String[] args) {
+        System.out.println(encrypt(text, 5));
+    }
+}
+}
