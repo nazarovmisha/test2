84601708b4c471055d5697c2f9d32418602aec25
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-10-19 20:20:43 +0300

    CheckResults

---
+++ Learn/src/CheckResults.java
@@ -0,0 +1,18 @@
+public class CheckResults {
+    private static int counter = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        new Thread(){
+            public void run(){
+                for (int i = 0; i < 100 ; i++) {
+                    CheckResults.counter++;
+                }
+            }
+        }.start();
+        while (CheckResults.counter<100){
+            System.out.println("Not reached yet");
+            Thread.sleep(1000);
+        }
+        System.out.println("Reached");
+    }
+}
