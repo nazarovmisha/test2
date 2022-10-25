e25967a6e7a26c0216f21db74bffa5d84cca1359
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-10-25 19:45:45 +0300

    Typer

---
+++ Learn/src/Typer/Main.java
@@ -0,0 +1,9 @@
+package Typer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Typer("Message first. ").start();
+        new Typer("Message second.").start();
+    }
+}
---
+++ Learn/src/Typer/Typer.java
@@ -0,0 +1,23 @@
+package Typer;
+
+public class Typer extends Thread {
+    static Object lock = new Object();
+    String message;
+
+    Typer(String message) {
+        this.message = message;
+    }
+
+    public void run(){
+        synchronized(lock){
+            for(int i = 0; i < message.length(); i++){
+                System.out.print(message.charAt(i));
+                try{
+                    Thread.sleep(500);
+                } catch (InterruptedException e){
+                    break;
+                }
+            }
+        }
+    }
+}
