dd3db165f3586d13032d74ef22a11facd2aac445
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-10-26 20:19:54 +0300

    Bank

--- Learn/src/Bank.java
+++ Learn/src/Bank.java
@@ -1,33 +1,38 @@
-package Bank;
+
 
 public class Bank {
 
     public static void main(String[] args) throws InterruptedException {
         Bank bank = new Bank();
-        while(true) {
+        while (true) {
             System.out.println(bank.getMoney());
             Thread.sleep(1000);
         }
     }
 
     private int money = 10000;
+    Object lock = new Object();
 
     int getMoney() {
         return money;
     }
 
     void take(int money) {
-        this.money -= money;
+        synchronized (lock) {
+            this.money -= money;
+        }
     }
 
     void repay(int money) {
-        this.money += money;
+        synchronized (lock) {
+            this.money += money;
+        }
     }
 
-    class Client extends Thread{
+    class Client extends Thread {
         @Override
         public void run() {
-            while(true) {
+            while (true) {
                 take(1000);
                 repay(1000);
             }
