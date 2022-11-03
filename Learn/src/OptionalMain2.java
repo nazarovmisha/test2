3aa51004a134ec54f84698ce6eaa9ee2fa530f9d
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-11-03 18:48:55 +0300

    OptionalMain2

--- Learn/src/OptionalMain2.java
+++ Learn/src/OptionalMain2.java
@@ -4,8 +4,6 @@ public class OptionalMain2 {
     public static void main(String[] args) {
 
         Optional<Double> average = OptionalMain.average(10,100);
-        if(average.isPresent()){
-            System.out.println(average.get());
-        }
+        average.ifPresent(System.out::println);
     }
 }
