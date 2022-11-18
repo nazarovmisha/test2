dab06b11b67bb3a54b8cf523dbcade84e2a49600
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-11-18 21:11:31 +0300

    Ex37

--- Learn/src/Ex37.java
+++ Learn/src/Ex37.java
@@ -1,14 +1,15 @@
 import java.util.LinkedList;
-//Напишите Java-программу для удаления и возврата первого элемента связанного списка.
-public class Ex38 {
+//Напишите Java-программу для извлечения, но не удаления, первого элемента связанного списка.
+public class Ex37 {
     public static void main(String[] args) {
         LinkedList<String> list = new LinkedList<>();
         list.add("Orange");
         list.add("Green");
         list.add("White");
         list.add("Black");
+
         System.out.println(list);
-        System.out.println("Removed element: " + list.pop());
+        System.out.println(list.peek());
         System.out.println(list);
     }
 }
