7bf5f8fe181a45e59b25a35f80ed10956187f172
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-11-21 20:02:42 +0300

    Ex51

---
+++ Learn/src/Ex51.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+//Напишите Java-программу для преобразования хэш-набора в набор деревьев.
+public class Ex51 {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        set.add("Orange");
+        set.add("Green");
+        set.add("White");
+        set.add("White");
+        set.add("Black");
+        Set<String> newSet = new TreeSet<>();
+        newSet.addAll(set);
+        for (String e : newSet)
+            System.out.println(e);
+    }
+}
