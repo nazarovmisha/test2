95f126bf4f96dd5481f02739b2c441a87bd33b08
Nazarov Mikhail <nazarovmisha@inbox.ru>
2022-11-21 20:05:03 +0300

    Ex53

---
+++ Learn/src/Ex53.java
@@ -0,0 +1,18 @@
+import java.util.Iterator;
+import java.util.TreeSet;
+
+//Напишите программу на Java для создания представления в обратном порядке элементов, содержащихся в данном наборе деревьев.
+public class Ex53 {
+    public static void main(String[] args) {
+        TreeSet<String> set = new TreeSet<>();
+        set.add("Orange");
+        set.add("Green");
+        set.add("White");
+        set.add("White");
+        set.add("Black");
+        Iterator it = set.descendingIterator();
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+}
